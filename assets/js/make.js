3c5f1d09d3566771f8caccd9464f63640b02491b
JACJava <juliejava@icloud.com>
2019-06-16 21:51:24 -0500

    AWS Code updates for S3

--- assets/js/make.js
+++ assets/js/make.js
@@ -15,7 +15,7 @@ function loadImages () {
   $.each(toppings, function (key, val) {
     var img = new Image();
     img.setAttribute('crossOrigin', 'anonymous');
-    img.src = '/assets/toppings/' + val.image;
+    img.src = '//pizza-luvrs-julie-c.s3.us-east-2.amazonaws.com/toppings/' + val.image;
     val.img = img;
   });
 }
